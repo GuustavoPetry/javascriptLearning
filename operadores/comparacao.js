30778890f73f104a42140e9d8666b5e0245778b2
GuustavoPetry <ogustavopetry@gmail.com>
2025-04-08 08:56:01 -0300

    operadores de comparação e lógicos

---
+++ operadores/comparacao.js
@@ -0,0 +1,24 @@
+// Operadores de Comparação retornar TRUE ou FALSE:
+//  == -> igual(valor)
+    console.log(10 == 10); // true
+
+//  === -> estritamente igual(valor e tipo)
+    console.log(10 === '10'); // false
+
+//  != -> diferente(valor)
+    console.log(6 != 8); // true
+
+//  !== -> estritamente diferente(valor e tipo)
+    console.log(10 !== '10'); // true
+
+//  >  --maior que
+    console.log(10 > 5); // true
+
+//  <  --menor que
+    console.log(10 < 5); // false
+
+//  >=  --maior ou igual
+    console.log(20 >= 20); // true
+
+// <=  --menor ou igual
+    console.log(30 <= 30); // true
---
+++ operadores/logicos.js
@@ -0,0 +1,10 @@
+// Operadores Lógicos são usados para combinar expressões Booleanas:
+//  && -> operador 'AND'
+    console.log(10 > 5 && 10 > 20); // false
+
+//  || -> operador 'OR'
+    console.log(10 > 20 || 10 > 5); // true
+
+// ! -> operador 'NOT'
+    let boolean = !(true);
+    console.log(boolean); // false
