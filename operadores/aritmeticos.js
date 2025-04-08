b2945dc6522aadd719fa7c3bbeee14604c98902a
GuustavoPetry <ogustavopetry@gmail.com>
2025-04-08 08:33:50 -0300

    operadores aritméticos

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,66 @@
+/* Operadores Aritméticos são utilizados para realizar operações matemáticas*/
+    // + -> adição
+    function adicao(){
+        let x = 10, y = 2;
+        let soma = x + y;
+        console.log(soma);
+    } 
+    // adicao()
+
+    // - -> subtração
+    function subtracaoCalc(){
+        let x = 10, y = 2;
+        let subtracao = x - y;
+        console.log(subtracao);
+    } 
+    subtracaoCalc()
+
+    // * -> multiplicação
+    function multiplicacaoCalc(){
+        let x = 10, y = 2;
+        let multiplicacao = x * y;
+        console.log(multiplicacao);
+    }
+    multiplicacaoCalc()
+
+    // / -> divisão
+    function divisaoCalc(){
+        let x = 10, y = 2;
+        let divisao = x / 2;
+        console.log(divisao);
+    }
+    divisaoCalc()
+
+    // % -> módulo (resto da divisão)
+    function moduloCalc(){
+        let x = 10, y = 3;
+        let modulo = x % y;
+        console.log(modulo);
+    }
+    moduloCalc()
+
+    // ** -> exponenciação
+    function exponenciacaoCalc(){
+        let x = 3, y = 2;
+        let exponenciacao = x ** y;
+        console.log(exponenciacao);
+    }
+    exponenciacaoCalc()
+
+    // ++ -> incremento
+    function incrementar(){
+        let x = 1;
+        console.log(x);
+        x++;
+        console.log(x);
+    }
+    incrementar()
+
+    // -- -> decremento
+    function decrementar(){
+        let x = 3;
+        console.log(x);
+        x--;
+        console.log(x);
+    }
+    decrementar()
